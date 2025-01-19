65dbc581585ffe04d77d2b91cc878532250ee70e
aliftffd <vainvictory432@gmail.com>
2025-01-19 23:19:10 +0800

    mengubah config init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
+-- For Lua configuration (init.lua)
+vim.g.venvselect_stay_on_this_version = true
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
