8808f4a3c903df65f12f31edb67f8274813c6559
aliftffd <vainvictory432@gmail.com>
2025-01-20 13:48:56 +0800

    Mencoba memperbaik venv message pop up

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,19 +1,30 @@
 return {
     {
       "linux-cultist/venv-selector.nvim",
-      dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
+      dependencies = { 
+        "neovim/nvim-lspconfig", 
+        "nvim-telescope/telescope.nvim",
+        "mfussenegger/nvim-dap-python",
+       },
       config = function()
         require("venv-selector").setup({
-          name = ".venv",
-          auto_refresh = true
+          name = {
+            "venv",
+            ".venv",
+            "env",
+            ".env"
+          }, 
+          auto_refresh = true,
+          parents = 0,
         })
       end,
       event = "VeryLazy",
       keys = {
         { "<leader>vs", "<cmd>VenvSelect<cr>", desc = "Select VirtualEnv" },
+
       },
     },
-  
+    
     -- Debug adapter for Python
     {
       "mfussenegger/nvim-dap-python",
@@ -49,5 +60,76 @@ return {
           }
         })
       end,
+    },
+      -- Existing Python plugins...
+
+      -- Add LuaSnip for snippets
+      {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+            "rafamadriz/friendly-snippets",
+        },
+        config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+    },
+
+    -- Add nvim-cmp for completion
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-nvim-lsp-signature-help",
+        },
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<CR>"] = cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    }),
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                }),
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                    { name = "nvim_lsp_signature_help" },
+                },
+            })
+        end,
     },
   }
